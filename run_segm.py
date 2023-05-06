284e3807e0801a5de54b1fe07404cfdf0d16625b
Matthew Danish <m.r.danish@uu.nl>
2023-05-06 18:39:22 +0200

    add options for scaling down and cropping panoramic images

--- run_segm.py
+++ run_segm.py
@@ -2,6 +2,7 @@
 # Simple program to load an image, run a semantic segmentation model on it
 # (from MXNet model zoo) and save the results in a compressed numpy file.
 import argparse
+from time import time
 import numpy as np
 from pathlib import Path
 import sys
@@ -17,6 +18,9 @@ parser.add_argument('--verbose', '-v', action='store_true', default=False, help=
 parser.add_argument('--modelname', metavar='MODEL', help='Use a specified model (from gluoncv.model_zoo)',default='psp_resnet101_citys')
 parser.add_argument('--gpu', '-G', metavar='N', nargs='?', default=None, const=True, help='Use GPU (optionally specify which one)')
 parser.add_argument('--outputfile', '-o', metavar='FILENAME', default=None, help='Output filename (default is input filename changed to have ".npz" suffix)')
+parser.add_argument('--no-detect-panoramic', default=False, action='store_true',help='Do not try to detect and correct panoramic images')
+parser.add_argument('--scaledown-factor', '-s', default=4, type=float, help='Image scaling down factor')
+parser.add_argument('--scaledown-interp', default=3, type=int, help='Interpolation method (see mxnet.image.imresize docs)')
 
 
 def main():
@@ -40,6 +44,17 @@ def main():
 
     vlog(f'Loading image "{args.filename}".')
     img = image.imread(args.filename)
+    vlog(f'Image size={img.shape[1]}x{img.shape[0]}.')
+    if not args.no_detect_panoramic and img.shape[1] >= img.shape[0]*2:
+        img = image.fixed_crop(img, 0, 0, img.shape[1], img.shape[0]*3//4)
+        vlog(f'Assuming panoramic image, cropping to {img.shape[1]}x{img.shape[0]}.')
+
+    if args.scaledown_factor != 1:
+        img = image.imresize(img,
+                             int(img.shape[1]//args.scaledown_factor),
+                             int(img.shape[0]//args.scaledown_factor),
+                             interp=int(args.scaledown_interp))
+        vlog(f'Scaling down image to {img.shape[1]}x{img.shape[0]}.')
 
     img = test_transform(img, ctx)
 
@@ -47,9 +62,13 @@ def main():
     model = gluoncv.model_zoo.get_model(args.modelname, pretrained=True)
 
     vlog('Running model...')
-    output = model.predict(img)
+    t1 = time()
 
+    output = model.predict(img)
     predict = mx.nd.squeeze(mx.nd.argmax(output, 1)).asnumpy()
+
+    t2 = time()
+    vlog(f'Complete. Runtime: {(t2-t1):.2f}s.')
 
     if args.outputfile is None:
         outputfile = Path(args.filename).with_suffix('.npz')
